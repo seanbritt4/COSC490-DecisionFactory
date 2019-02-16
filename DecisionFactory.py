94c76d17fbafe95e7f9673db21a6bd0b4606cde9
seanbritt4 <seanbritt4@gmail.com>
2019-02-16 13:31:42 -0500

    Add files via upload

--- DecisionFactory.py
+++ DecisionFactory.py
@@ -14,7 +14,8 @@ class DecisionFactory:
 	def __init__(self, name='Walter Wanderley'):
 		self.name = name
 		self.directions = ['wait', 'up', 'down', 'right', 'left']
-		self.last_result = 'success'
+		self.failed_directions = []
+		self.last_result = 'start'
 		self.last_direction = 'wait'
 
 	#Note: we have relativistic coordinates recorded here, since the map
@@ -22,13 +23,43 @@ class DecisionFactory:
 	#self.state.pos = (0,0)
 
 	def get_decision(self, verbose = True):
-		return self.random_direction()
+		# return self.random_direction()
+		return self.better_than_random()
 
 	def random_direction(self):
 		#random.randint(0,4) #Includes wait state
 		r = random.randint(1,4) #Does NOT include wait state
 
 		return self.directions[r]
+
+	def better_than_random(self):
+		r = self.random_direction()
+
+		# print "Last result: ", self.last_result
+		# print "Last direction: ", self.last_direction
+
+		self.failed_directions.extend(self.last_direction)
+
+		if self.last_result != 'success':
+			self.failed_directions.extend(self.last_direction)
+
+			r = random.randint(1,4)
+
+			while self.directions[r] in self.failed_directions:
+				# print "Failed:", self.failed_directions
+				r = random.randint(1,4)
+
+			# print self.directions[r]
+			return self.directions[r]
+		
+		else:	
+			self.failed_directions = []
+
+
+			# r = self.last_direction
+			r = self.random_direction()
+			# print r
+			return r
 
 	def put_result(self, result):
 		self.result = result
