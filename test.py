9db100678b1a67c5649a9869c43b9d776b8c965d
seanbritt4 <seanbritt4@gmail.com>
2019-02-25 08:18:22 -0500

    Fixed command line map file regex search

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-import sys
+import re
 
-for arg in sys.argv:
-	#print arg
-	if arg == '-ng' or arg == '-noGraphics':
-		print 'no grphics'
-	elif arg == "-initOverride" or arg == "-r":
-		print 'over ride'
-	elif arg == "-fast" or arg == "-f":
-		print 'gotta go fast'
+s = "map00.txt"
 
+x = re.search("map.*\.txt", s)
+
+if x:
+	print x.string
+else:
+	print 'Not Found'
